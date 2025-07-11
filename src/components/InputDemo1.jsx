ddee80ef1ff9e91e69ef140d9b08f569de225fb4
samirvithlani <samir.vithlani83955@gmail.com>
2025-07-11 16:50:19 +0530

    date

--- src/components/InputDemo1.jsx
+++ src/components/InputDemo1.jsx
@@ -5,6 +5,7 @@ export const InputDemo1 = () => {
     const [name, setname] = useState("")
     const [age, setage] = useState("")
     const [isSubbmited, setisSubbmited] = useState(false)
+    const [dateLable, setdateLable] = useState("")
     //name == vr
     //setName -->function
     //"" inital value
@@ -16,6 +17,39 @@ export const InputDemo1 = () => {
     const submitHandler =()=>{
         setisSubbmited(true)
     }
+
+    const dateHandler = (event) => {
+        const selectedDate = new Date(event.target.value); 
+        const today = new Date(); //today
+    
+    
+        selectedDate.setHours(0,0,0,0);
+        today.setHours(0,0,0,0);
+    
+        const msInDay = 24 * 60 * 60 * 1000;
+        console.log(msInDay)
+        const diffDays = (selectedDate - today) / msInDay;
+    
+        console.log("Difference in days:", diffDays);
+    
+        if (diffDays === 0) {
+            //console.log("Today");
+            setdateLable("Today")
+        } else if (diffDays === -1) {
+            //console.log("Yesterday");
+            setdateLable("Yesterday")
+        } else if (diffDays === 1) {
+            console.log("Tomorrow");
+            setdateLable("Tomorrow")
+        } else {
+            console.log(`Selected date is ${Math.abs(diffDays)} days ${diffDays < 0 ? 'ago' : 'ahead'}`);
+            setdateLable(`Selected date is ${Math.abs(diffDays)} days ${diffDays < 0 ? 'ago' : 'ahead'}`)
+        }
+    };
+    
+    
+    
+    
 
   return (
     <div style={{textAlign:"center"}}>
@@ -31,6 +65,9 @@ export const InputDemo1 = () => {
             {/* {age} */}
         </div>
         <div>
+            <input type='date' onChange={(event)=>{dateHandler(event)}}></input>
+        </div>
+        <div>
             <input type='color'></input>
         </div>
         <div>
@@ -41,6 +78,7 @@ export const InputDemo1 = () => {
             <h1>OUTPUT</h1>
             <h1>NAME = {name}</h1>
             <h1>AGe = {age}</h1>
+            <h1>Date Lable = {dateLable}</h1>
         </div>
         }
         
