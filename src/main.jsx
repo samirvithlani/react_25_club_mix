419e06967b198b18fde098eacf716135ea512029
samirvithlani <samir.vithlani83955@gmail.com>
2025-08-25 14:20:06 +0530

    redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import { store } from './store.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { persistor, store } from "./store.jsx";
+import { PersistGate } from "redux-persist/integration/react";
 //import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+    <PersistGate persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
-)
+);
--- src/store.jsx
+++ src/store.jsx
@@ -1,12 +1,42 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./redux/CartSlice"
 import bankReducer from "./redux/BankSlice"
 import contentReducer from "./redux/ContentSlice"
+import storage from "redux-persist/lib/storage"
+import { persistReducer, persistStore, PURGE } from "redux-persist";
+
+// export const store = configureStore({
+//     //reducers
+//     reducer:{
+//         cart:cartReducer,
+//         bank:bankReducer,
+//         content:contentReducer
+//     }
+// })
+
+
+
+const rootReducer = combineReducers({
+         cart:cartReducer,
+         bank:bankReducer,
+         content:contentReducer
+})
+
+const persistConfig ={
+    key:"root",
+    storage,
+    whitelist:["bank"]
+}
+
 export const store = configureStore({
-    //reducers
-    reducer:{
-        cart:cartReducer,
-        bank:bankReducer,
-        content:contentReducer
+
+    reducer:persistReducer(persistConfig,rootReducer),
+     middleware:(getDefaultMiddleware)=>{
+        return getDefaultMiddleware({
+            serializableCheck:{
+                ignoreActions:[PURGE]
+            }
+        })
     }
 })
+export const persistor =persistStore(store)
